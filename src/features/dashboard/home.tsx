c3dabd9f8ae5c7100104fd8f2af3782cc2d99f5d
Daniel Babalola <dannybabs123456@gmail.com>
2024-10-21 23:36:27 -0400

    feat:Finished Main Dahsboard Overview Ui and made it responsive

--- src/features/dashboard/home.tsx
+++ src/features/dashboard/home.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import AiSearch from './components/ai-search';
+import { Component } from './components/horizontal-charts';
+import { UpcomingMeetings } from './components/schedule-list';
+import TaskSummary from './components/task-list';
+import CallHistoryTable from './components/recent-calls';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 export default function Home() {
     return (
-        <div className="p-4 space-y-0.5">
-            <h1 className="text-xl font-semibold text-text-dark">Home</h1>
-            {/*
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbItem>
-                            <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator />
-                        <BreadcrumbItem>
-                            <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator />
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>Home</BreadcrumbPage>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>  */
-            }
+        <div className="md:p-4 p-2 ">
+
+            <Breadcrumb className=' ml-2 '>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href="#">Home</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Dashboard</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <h1 className="text-2xl font-semibold text-text-dark ml-2 mt-2 mb-4">Dashboard</h1>
+
+            {/* Main Content*/}
+            <div className="flex flex-1 flex-col gap-4 md:p-2 ">
+                <div className="grid gap-4 md:grid-cols-3">
+                    {/* Ai Search Component*/}
+                    <div className="flex flex-col md:col-span-2 gap-2.5">
+                        <AiSearch />
+                    </div>
+                    {/* Sales Snapshot Charts */}
+                    <div className="flex flex-col md:col-span-1 gap-2">
+                        <Component />
+                    </div>
+                </div>
+                <div className=" grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <TaskSummary />
+                    <UpcomingMeetings />
+                </div>
+
+                <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" >
+                    <CallHistoryTable />
+                </div>
+
+            </div>
         </div>
     );
 }
